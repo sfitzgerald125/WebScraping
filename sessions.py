ceaf343eb250519c522054e78312eff82690fd4a
Spencer Fitzgerald <spencerfitz0196@gmail.com>
2019-04-18 11:04:01 -0600

    remove unused files

--- sessions.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-
-with requests.Session() as s:
-    r = s.get('https://www.utahrealestate.com/search/perform/md/undefined/recent/1', headers=headers)
-    soup = BeautifulSoup(r.content, 'lxml')
-    print(soup.prettify())
